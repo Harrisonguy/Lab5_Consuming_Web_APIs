cc84f99e7000eaa012d1061d20e9581ca9441277
Harrisonguy <hguy@flemingcollege.ca>
2023-02-14 23:04:32 -0500

    ready for lab

---
+++ pastbin_api.py
@@ -0,0 +1,17 @@
+def post_new_paste(title, body_text, expiration='N', listed=True):
+    """
+    Posts a new paste to PasteBin
+
+    :param title: Paste title
+    :param body_text: Paste body text
+    :param expiration date of paste (n - never, 10M - minutes, 1H, 1D, 1W, 2W, 1M, 6M, 1Y)
+    :param listed: Wheter paste is publicly listed (True) or not (False)
+    :returns: URL of new paste, if successful. Otherwise None.
+    """
+    
+    
+    
+    
+    
+    
+    return
